01758d7edc5c214d70118866cde05de3becb4683
Dineth Kodippili <125392282+dithnim@users.noreply.github.com>
2023-10-18 20:37:55 +0530

    new file:   WG-new/apis/post_email.php

---
+++ WG-new/apis/post_email.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once( 'connection_user.php' );
+require_once( '../functions/send_requests.php' );
+
+if ($_SERVER[ 'REQUEST_METHOD' ] == "POST")
+{
+    $jsondata = file_get_contents( "php://input" );
+    $data     = json_decode( $jsondata, true );
+
+    if (isset( $data ))
+    {
+        $email = $data[ "email" ];
+        $stmt = $conn->prepare( "INSERT INTO `email_notifies` (`email`) VALUES (?);" );
+        $stmt->bind_param( "s", $email );
+
+        echo "hii";
+        $stmt->execute();
+
+        if ($stmt->error)
+        {
+            sendres( "500", "", $stmt->error );
+            $stmt->close();
+            $conn->close();
+        }
+        else
+        {
+            $stmt->close();
+            $conn->close();
+        }
+        
+    }
+}
+?>
